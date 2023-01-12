83d0176b7b3aaefdbc4949bb411a9cd749e0cf43
taewan <taewan2002@naver.com>
2023-01-12 20:15:44 +0900

    이미지, 영상 크기 조정 하기

--- PythonImageWorkspace/OpenCV/test.py
+++ PythonImageWorkspace/OpenCV/test.py
@@ -1,33 +1,10 @@
-import time
-import io
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image, ImageFile
-ImageFile.LOAD_TRUNCATED_IMAGES = True
-from rembg.bg import remove as remove_bg
-import cv2
-def show_rembg(path):
-    fig = plt.figure(figsize=(10, 10))
+from rembg import remove
+from PIL import Image
 
-    # show original image
-    fig.add_subplot(1, 2, 1)
-    orig_img = Image.open(path)
-    plt.imshow(orig_img)
+input_path = 'car.jpeg'
+output_path = 'output.png'
 
-    # show bg removed image
-    fig.add_subplot(1, 2, 2)
-    f = np.fromfile(path)
+input = Image.open(input_path)
+output = remove(input)
+output.save(output_path)
 
-    started = time.time()
-    result = remove_bg(f)
-    elapsed = time.time() - started
-    print(f'it takes {elapsed} seconds for removing bg.')
-
-    # img = Image.open(io.BytesIO(result)).convert("RGBA")
-    img = Image.frombytes('RGBA', (128,128), result, 'raw')
-    plt.imshow(img)
-    plt.show()
-    cv2.imwrite('translated.png', img)
-
-# Usage
-show_rembg('car.jpeg')
